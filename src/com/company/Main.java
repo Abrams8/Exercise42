e8c3ba150753989896fa521a17b9239a11986fa0
Abrams <dima.abramchik@gmail.com>
2021-06-28 17:56:09 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         int repeat = 0;
+        int i = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку: ");
         String str = in.nextLine();
-        String[] words = str.split("[^а-яА-Я]");
+        String[] words = str.split("[^а-яА-ЯA-Za-z0-9]+");
 
-        Scanner inTwo = new Scanner(System.in);
-        System.out.println("Введите слово: ");
-        String findWord = inTwo.nextLine();
-
-        Map<String, Integer> check = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            if (findWord.isEmpty() == false && findWord.equals(words[i]) == true) {
-                check.put(findWord, ++repeat);
+        Map<String, Integer> check = new LinkedHashMap<>();
+        for (String num : words) {
+            Integer counter = check.get(num);
+            if (counter == null) {
+                counter = 0;
             }
+            check.put(num, counter + 1);
         }
-        System.out.println("Количество повторений слова " + findWord + ": " + check.get(findWord));
+        System.out.println(check);
     }
 }
